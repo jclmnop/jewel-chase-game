39e44c2fca64fb61c7084b5d8a5323b28a71982b
Dillon Ward <dillon.ward.uk@gmail.com>
2022-11-17 20:27:30 +0000

    Basic A* Implementation

--- src/Entities/Characters/Npc/SmartThief.java
+++ src/Entities/Characters/Npc/SmartThief.java
@@ -1,5 +1,9 @@
 package Entities.Characters.Npc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 import DataTypes.CollisionType;
 import Entities.Characters.Character;
 
@@ -13,6 +17,34 @@ public class SmartThief extends Npc {
         //TODO: implement
     }
 
+    /**
+     * Calculates the shortest path to an item using A*.
+     * @param hm The graph of tiles.
+     * @param start The tile the smart thief is currently on.
+     * @param goal The tile containing the desired item.
+     */
+    private void findShortestPath(HashMap<Object, ArrayList<Object>> hm, Object start, Object goal) {
+
+        // Queues nodes to be examined.
+        PriorityQueue frontier = new PriorityQueue<Object>();
+        frontier.add(start);
+
+        // Loop terminates when the goal is reached and therefore the shortest path has been found.
+        while (frontier.peek() != goal) {
+            Object current = frontier.poll();
+            for (Object neighbour : hm.get(current)) {
+                int cost = current.distance + 1; // Movement to the next node is always 1.
+                // Checks if shorter path to neighbouring node has been found.
+                if (cost < neighbour.distance) {
+                    neighbour.distance = cost;
+                    neighbour.parent = current;
+                    if (!frontier.contains(neighbour)) {
+                        frontier.add(neighbour);
+                    }
+                }
+            }
+        }
+    }
     /**
      * Serialises the Object into a String.
      *
