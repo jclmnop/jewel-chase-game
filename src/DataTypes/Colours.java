7ff0f2abd56799fe02559a26fe1f48ab8298ebd0
jclmnop <jcheshire@protonmail.com>
2022-11-18 16:40:19 +0000

    i just discovered 'record' in java lol

--- src/DataTypes/Colours.java
+++ src/DataTypes/Colours.java
@@ -3,35 +3,7 @@ package DataTypes;
 /**
  * Represents an immutable tuple of 4 Colours (Java doesn't have tuples).
  */
-public class Colours {
-    private final Colour c1;
-    private final Colour c2;
-    private final Colour c3;
-    private final Colour c4;
-
-    public Colours(Colour c1, Colour c2, Colour c3, Colour c4) {
-        this.c1 = c1;
-        this.c2 = c2;
-        this.c3 = c3;
-        this.c4 = c4;
-    }
-
-    public Colour getC1() {
-        return c1;
-    }
-
-    public Colour getC2() {
-        return c2;
-    }
-
-    public Colour getC3() {
-        return c3;
-    }
-
-    public Colour getC4() {
-        return c4;
-    }
-
+public record Colours(Colour c1, Colour c2, Colour c3, Colour c4) {
     @Override
     public String toString() {
         return "" + c1.toChar() + c2.toChar() + c3.toChar() + c4.toChar();
