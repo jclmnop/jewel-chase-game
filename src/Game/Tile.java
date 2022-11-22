d25010c66da23f0938ae64811919d70e24223a29
jclmnop <jcheshire@protonmail.com>
2022-11-22 15:33:50 +0000

    add buildNoColourAdjacencyMap() method to Tile and implement getSingleColourAdjacentTiles() method

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -13,12 +13,12 @@ import java.util.HashMap;
 public class Tile implements Serialisable {
     // Static attributes //
     private static Tile[][] board;
-    // Adjacency map for FloorFollowingThief
-    private static HashMap<Coords, AdjacentTiles> singleColourAdjacencyMap;
     // Adjacency map for SmartThief and Player
     private static HashMap<Coords, AdjacentTiles> multiColourAdjacencyMap;
     // Adjacency map for FlyingAssassin
     private static HashMap<Coords, AdjacentTiles> noColourAdjacencyMap;
+    private static int height;
+    private static int width;
 
     // Instance attributes //
     private final Colours colours;
@@ -29,8 +29,12 @@ public class Tile implements Serialisable {
     }
 
     // Static methods //
-    public static AdjacentTiles getSingleColourAdjacentTiles(Coords coords) {
-        return singleColourAdjacencyMap.get(coords);
+    public static AdjacentTiles getSingleColourAdjacentTiles(Coords coords,
+                                                             Colour colour) {
+        return AdjacentTiles.singleColourAdjacentTiles(
+            noColourAdjacencyMap.get(coords),
+            colour
+        );
     }
 
     public static AdjacentTiles getMultiColourAdjacentTiles(Coords coords) {
@@ -45,6 +49,10 @@ public class Tile implements Serialisable {
         return board[coords.y()][coords.x()];
     }
 
+    public static boolean isValidCoords(Coords coords) {
+        return Tile.isValidX(coords.x()) && Tile.isValidY(coords.y());
+    }
+
     public static void move(Entity entity, Coords from, Coords to) {
         var fromTile = Tile.getTile(from);
         var toTile = Tile.getTile(to);
@@ -53,7 +61,48 @@ public class Tile implements Serialisable {
         entity.setCoords(to);
     }
 
-    public static void newBoard(Tile[][] board) {
+    // TODO: buildMultiColourAdjacencyMap
+
+    public static void buildNoColourAdjacencyMap() {
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                Tile up = null;
+                Tile down = null;
+                Tile left = null;
+                Tile right = null;
+
+                Coords currentCoords = new Coords(col, row);
+                Coords upCoords = new Coords(col, row - 1);
+                Coords downCoords = new Coords(col, row + 1);
+                Coords leftCoords = new Coords(col - 1, row);
+                Coords rightCoords = new Coords(col + 1, row);
+
+                // Leave tile as null if coords are invalid
+                if (Tile.isValidCoords(upCoords)) {
+                    up = Tile.getTile(upCoords);
+                }
+
+                if (Tile.isValidCoords(downCoords)) {
+                    down = Tile.getTile(downCoords);
+                }
+
+                if (Tile.isValidCoords(leftCoords)) {
+                    left = Tile.getTile(leftCoords);
+                }
+
+                if (Tile.isValidCoords(rightCoords)) {
+                    right = Tile.getTile(rightCoords);
+                }
+
+                AdjacentTiles adjacentTiles = new AdjacentTiles(up, down, left, right);
+                Tile.noColourAdjacencyMap.put(currentCoords, adjacentTiles);
+            }
+        }
+    }
+
+    public static void newBoard(Tile[][] board, int height, int width) {
+        Tile.height = height;
+        Tile.width = width;
         Tile.board = board;
     }
 
@@ -110,5 +159,13 @@ public class Tile implements Serialisable {
     @Override
     public String serialise() {
         return this.colours.toString();
+    }
+
+    private static boolean isValidX(int x) {
+        return x >= 0 && x < width;
+    }
+
+    private static boolean isValidY(int y) {
+        return y >= 0 && y < height;
     }
 }
