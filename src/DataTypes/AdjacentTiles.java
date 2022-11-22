f1c0b411e636ffb8c3619d27f00596fefd47efde
jclmnop <jcheshire@protonmail.com>
2022-11-22 15:31:41 +0000

    add singleColourAdjacentTiles(adjacentTiles, colour) method to AdjacentTiles datatype

--- src/DataTypes/AdjacentTiles.java
+++ src/DataTypes/AdjacentTiles.java
@@ -2,4 +2,18 @@ package DataTypes;
 
 import Game.Tile;
 
-public record AdjacentTiles(Tile up, Tile down, Tile left, Tile right) {}
+/**
+ * Contains adjacent tiles for a specific coordinate. If value is null, then
+ * coordinate has no adjacent tiles in that direction.
+ */
+public record AdjacentTiles(Tile up, Tile down, Tile left, Tile right) {
+    public static AdjacentTiles singleColourAdjacentTiles(AdjacentTiles adjacentTiles,
+                                                          Colour colour) {
+        Tile up = (adjacentTiles.up.hasColour(colour)) ? adjacentTiles.up : null;
+        Tile down = (adjacentTiles.down.hasColour(colour)) ? adjacentTiles.down : null;
+        Tile left = (adjacentTiles.left.hasColour(colour)) ? adjacentTiles.left : null;
+        Tile right = (adjacentTiles.right.hasColour(colour)) ? adjacentTiles.right : null;
+
+        return new AdjacentTiles(up, down, left, right);
+    }
+}
