7aae6f23e8f4f7e893bae19c83dcb30a637da538
jclmnop <jcheshire@protonmail.com>
2022-11-25 15:29:18 +0000

    game TODOs

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,27 @@
 package Game;
 
 public class Game {
-    //TODO:
+    //TODO: -- variables --
+    //TODO: score
+    //TODO: timeRemaining
+    //TODO: TICKS_PER_SECOND
+    //TODO: running (bool)
+
+    //TODO: -- public --
+    //TODO: getScore()
+    //TODO: adjustScore()
+    //TODO: startGame()
+    //TODO: endGame()
+    //TODO: quit()
+    //TODO: getTimeRemaining()
+    //TODO: adjustTime()
+    //TODO: loadGame()
+    //TODO: saveGame()
+    //TODO: isRunning()
+
+    //TODO: -- private --
+    //TODO: tick()
+    //TODO: movePlayer()
+    //TODO: moveNpcs()
+    //TODO: resetGame()
 }
