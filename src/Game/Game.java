8ceab0d4559a37417599ad2747a7d147218ab38f
jclmnop <jcheshire@protonmail.com>
2022-11-25 18:46:29 +0000

    england vs usa is starting soon

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,12 +6,16 @@ import Entities.Characters.Player;
 import Entities.Entity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.time.Instant;
 
 public class Game {
-    public static final int TICKS_PER_SECOND = 100; //TODO: figure out reasonable value
+    public static final long MILLI_PER_TICK = 100; //TODO: figure out reasonable value
     private static int score = 0;
     private static int timeRemaining = 0;
     private static boolean running = false;
+    private static Instant lastTickTime;
 
     private Game() {};
 
@@ -51,23 +55,50 @@ public class Game {
     //TODO: quit()
     //TODO: win()
     //TODO: lose()
-
-    //TODO: -- maybe private idk --
     //TODO: loadGame()
     //TODO: saveGame()
 
     //TODO: -- private --
     //TODO: tick()
+
+    private static void tick(HashMap<Player, Direction> playerInputs) {
+        // TODO: call every function that needs to be called per tick
+        Game.moveNpcs();
+        // TODO: figure out how to get keyboard inputs passed to this function
+        //          - will also need a way to make sure player can't move *every* tick
+        //              - probably best to implement movement speed etc in player.tryMove()
+        //                then this function can just blindly pass keyboards inputs etc
+        //          - maybe parseInput() function?
+        // TODO: if we do co-op mode, need to differentiate between player1&2 keyboard inputs
+        for (Player player : playerInputs.keySet()) {
+            Game.movePlayer(player, playerInputs.get(player));
+        }
+        Entity.processCollisions(); // Collisions must be processed after movements
+
+        // TODO: time management (increment time after X ticks)
+        // TODO: measure time (delay of Y seconds at end of tick)
+        //                      Y: depends on how long tick takes to execute
+        Game.timeTick();
+    }
+
+    private static void timeTick() {
+        var now = Instant.now();
+        //TODO: what
+    }
+
     private static void moveNpcs() {
         ArrayList<Npc> npcs = Entity.filterEntitiesByType(
             Npc.class,
             Entity.getEntities()
         );
+        // TODO: implement tryMove for each npc() + make sure they can only move
+        //       a certain number of times per second based on speed
         npcs.forEach(Npc::tryMove);
     }
 
     private static void movePlayer(Player player, Direction direction) {
-        //TODO: implement player.tryMove(direction)
+        //TODO: implement .tryMove() for player
+        player.tryMove(direction);
     }
 
     private static void resetGame() {
