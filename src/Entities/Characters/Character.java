102d84591b297c38e896bff3aab786d73e13fecc
jclmnop <jcheshire@protonmail.com>
2022-11-26 16:31:55 +0000

    partial implementation of character.kill()

--- src/Entities/Characters/Character.java
+++ src/Entities/Characters/Character.java
@@ -4,12 +4,20 @@ import DataTypes.CollisionType;
 import DataTypes.Coords;
 import DataTypes.Direction;
 import Entities.Entity;
+import Game.Tile;
 
 public abstract class Character extends Entity {
     protected Direction currentDirection;
     protected Coords coords;
+    //TODO: sprite/image file?
+    //TODO: death animation?
 
     public Character(CollisionType collisionType, boolean isBlocking) {
         super(collisionType, isBlocking);
+    }
+
+    public void kill() {
+        // TODO: death animation?
+        Entity.removeEntity(this);
     }
 }
