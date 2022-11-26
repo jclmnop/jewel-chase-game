3347f0ecffd7d17c264716e9f336f9cfb42dae82
dwardy <61111939+dillon-ward@users.noreply.github.com>
2022-11-26 11:30:43 +0000

    Add files via upload

---
+++ src/Entities/Characters/SmartThief.java
@@ -0,0 +1,106 @@
+package Entities.Characters;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import DataTypes.*;
+import Entities.Items.*;
+import Game.Tile;
+
+public class SmartThief extends Character {
+
+    // Current desired item.
+    private Item item;
+
+    // Current path to item
+    private LinkedList<Coords> path;
+
+    public SmartThief() {
+        super(CollisionType.THIEF, true);
+    }
+    
+    /**
+     * Moves the smart thief by setting its coordinates to the head of the path queue.
+     * If path queue is empty or desired item is no longer available, the findPath method is called to construct a new path.
+     */
+    public void move() {
+        /* 
+            TODO - write static methods in Item class to return lists of available items.
+                 - examples here are default getItems and getCollectibles which just includes loot and levers.
+                 - loot and levers can be subclasses of a new class Collectible.
+        */
+        if (path.isEmpty() || !Item.getItems().contains(item)) {
+            // If no collectibles are available, smart thief will move to the nearest.
+            if (Item.getCollectibles().isEmpty()) {
+                findPath(Door.class);
+            } else {
+                findPath(Collectible.class);
+            }
+        }
+        coords = path.poll();
+    }
+
+    /**
+     * Finds shortest path to nearest item with BFS.
+     * @param item item type to search for.
+     */
+    private void findPath(Class itemType) {
+
+        // Instance represents a node in a BFS tree.
+        class Node {
+            public Coords coords;
+            public Node parent;
+
+            public Node (Coords c) {
+                coords = c;
+            }
+        }
+
+        // Implementation of queue in BFS algorithm.
+        LinkedList<Node> queue = new LinkedList<Node>();
+        queue.add(new Node(coords));
+
+        // Stores coords that of nodes that have been examined, so that don't get added to the queue again.
+        HashSet<Coords> foundCoords = new HashSet<Coords>();
+
+        // Run BFS until the node with a required item is found.
+        while (Tile.getTile(queue.peek().coords).getEntitiesOfType(itemType).isEmpty()) {
+            Node currNode = queue.poll();
+            foundCoords.add(currNode.coords);
+            AdjacentCoords adjCoords = Tile.getMultiColourAdjacentTiles(currNode.coords);
+            Coords[] coordsArr = {
+                                    adjCoords.getCoordsInDirection(Direction.UP),
+                                    adjCoords.getCoordsInDirection(Direction.DOWN),
+                                    adjCoords.getCoordsInDirection(Direction.LEFT),
+                                    adjCoords.getCoordsInDirection(Direction.RIGHT)
+                                };
+
+            // Add adjacent nodes to the queue and set parent pointers.
+            for (Coords c : coordsArr) {
+                if (c != null && !foundCoords.contains(c)) {
+                    Node n = new Node(c);
+                    n.parent = currNode;
+                    queue.add(n);
+                }
+            }
+        }
+
+        // Backtrack using parent pointers to construct the SmartThief's path.
+        Node n = queue.poll();
+        while (n.parent != null) {
+            path.addFirst(n.coords);
+            n = n.parent;
+        }
+    }
+
+    /**
+     * Serialises the Object into a String.
+     *
+     * @return Serialised string for `this` Object.
+     */
+    @Override
+    public String serialise() {
+        // TODO
+        return null;
+    }
+}
