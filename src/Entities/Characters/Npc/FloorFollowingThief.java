59247c828a9eb380b145a74950e2a6cd582d603f
Dillon Ward <dillon.ward.uk@gmail.com>
2022-12-01 17:21:33 +0000

    Implement FFT movement.

--- src/Entities/Characters/Npc/FloorFollowingThief.java
+++ src/Entities/Characters/Npc/FloorFollowingThief.java
@@ -1,17 +1,46 @@
 package Entities.Characters.Npc;
 
+
+import DataTypes.AdjacentCoords;
 import DataTypes.CollisionType;
+import DataTypes.Colour;
 import DataTypes.Coords;
+import DataTypes.Direction;
 import Entities.Characters.Character;
+import Game.Tile;
 
 public class FloorFollowingThief extends Npc {
-    public FloorFollowingThief(Coords coords, int speed) {
+
+    private final Colour colour;
+
+    public FloorFollowingThief(Coords coords, int speed, Colour col) {
         super(CollisionType.THIEF, true, coords, speed);
+        colour = col;
     }
 
+    /**
+     * Trys to move to tile with assigned colour that is left most to FFT's current direction.
+     */
     @Override
     public void tryMove() {
-        //TODO: implement
+        AdjacentCoords adjCoords = Tile.getSingleColourAdjacentTiles(coords, colour);
+        Direction dir = Direction.turnLeft(currentDirection);
+
+        /* Checks all directions, starting left of FFT and going clockwise.
+         * If there are no tiles to move to, FFT stays where it is.
+        */
+        boolean foundTile = false;
+        int i = 0;
+        while (!foundTile || i < 4) {
+            if (adjCoords.getTileInDirection(dir) != null) {
+                coords = adjCoords.getCoordsInDirection(dir);
+                currentDirection = dir;
+                foundTile = true;
+            } else {
+                dir = Direction.turnRight(dir);
+                i++;
+            }
+        }
     }
 
     /**
