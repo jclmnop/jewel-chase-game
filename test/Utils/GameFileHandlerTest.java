f63eedecfe0d0f5c06cc43017f0d550bba3b17c5
jclmnop <jcheshire@protonmail.com>
2022-12-05 16:40:07 +0000

    basic unit tests for loading levels and player profiles

--- test/Utils/GameFileHandlerTest.java
+++ test/Utils/GameFileHandlerTest.java
@@ -1,2 +1,64 @@
-package Utils;public class GameFileHandlerTest {
+package Utils;
+
+import DataTypes.Coords;
+import DataTypes.Exception.DeserialiseException;
+import Entities.Characters.Npc.FloorFollowingThief;
+import Entities.Characters.Npc.SmartThief;
+import Entities.Characters.Player;
+import Entities.Entity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import Game.Game;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GameFileHandlerTest {
+    public static final String PLAYER_NAME = "test";
+
+    @Test @BeforeEach
+    public void testLoadPlayerProfile() throws IOException {
+        GameFileHandler.loadPlayerProfile(PLAYER_NAME);
+        Assertions.assertEquals(PLAYER_NAME, Game.getPlayerProfile().getPlayerName());
+
+    }
+
+    @Test
+    public void testLoadLevelNoExceptions() throws IOException {
+        Assertions.assertEquals(PLAYER_NAME, Game.getPlayerProfile().getPlayerName());
+        ArrayList<String> levels = GameFileHandler.getAvailableLevels(Game.getPlayerProfile());
+
+        Assertions.assertEquals(
+            Game.getPlayerProfile().getMaxLevel(),
+            Integer.parseInt(levels.get(levels.size()-1))
+        );
+
+
+        try {
+            GameFileHandler.loadLevelFile(Integer.parseInt(levels.get(0)), Game.getPlayerProfile());
+        } catch (DeserialiseException deserialiseException) {
+            System.out.println(deserialiseException.getMessage());
+            Exception originalException = deserialiseException.getOriginalException();
+            if (originalException.getMessage() == null) {
+                System.out.println(originalException);
+            } else {
+                System.out.println(originalException.getMessage());
+            }
+            throw deserialiseException;
+        }
+
+        // Three entities are loaded
+        Assertions.assertEquals(3, Entity.getEntities().size());
+        // Entities have correct coordinates and appear in same order as file
+        Assertions.assertEquals(new Coords(0, 0), Entity.getEntities().get(0).getCoords());
+        Assertions.assertEquals(new Coords(5, 5), Entity.getEntities().get(1).getCoords());
+        Assertions.assertEquals(new Coords(3, 4), Entity.getEntities().get(2).getCoords());
+        // Entities have correct types
+        Assertions.assertTrue(Entity.getEntities().get(0) instanceof Player);
+        Assertions.assertTrue(Entity.getEntities().get(1) instanceof SmartThief);
+        Assertions.assertTrue(Entity.getEntities().get(2) instanceof FloorFollowingThief);
+
+    }
+
 }
