b50df3715fc4cd2420d40150476781c95bfa2627
jclmnop <jcheshire@protonmail.com>
2022-12-06 12:52:33 +0000

    add level select menu

--- src/App/App.java
+++ src/App/App.java
@@ -1,28 +1,25 @@
 package App;
 
-import DataTypes.Colour;
-import DataTypes.Coords;
-import DataTypes.Direction;
 import DataTypes.Exception.ParseBoardException;
 import DataTypes.Exception.ParseTileColourException;
 import DataTypes.GameParams;
-import Entities.Characters.Npc.FloorFollowingThief;
-import Entities.Characters.Player;
-import Utils.BoardLoader;
 import Utils.GameFileHandler;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import Game.Game;
-import Game.Tile;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,12 +30,13 @@ public class App extends Application {
     public static final int WINDOW_HEIGHT = 800;
     public static final String MENU_FXML_PATH = "fxml/menu.fxml";
     public static final String GAME_FXML_PATH = "fxml/game.fxml";
+    public static final String LEVEL_SELECT_FXML_PATH = "fxml/levelSelect.fxml";
     public static final String RESOURCES_PATH = "src/App/resources/";
     public static final String BRODYQUEST_MP3_PATH = RESOURCES_PATH + "brodyquest.mp3";
     public static final String ANACONDA_MP3_PATH = RESOURCES_PATH + "anaconda.mp3";
     public static final String[] TRACKS = {BRODYQUEST_MP3_PATH, ANACONDA_MP3_PATH};
-    public static final double DEFAULT_VOLUME = 0.3; // 50%
-    private static int currentTrack = 0;
+    public static final double DEFAULT_VOLUME = 0.3; // 30%
+    private static int currentAudioTrack = 0;
     private static Stage stage;
     private static MediaPlayer musicPlayer;
     private static App app;
@@ -71,6 +69,13 @@ public class App extends Application {
 
     public static void returnToMainMenu() throws IOException {
         App.app.changeScene(MENU_FXML_PATH);
+        App.updateMessageOfTheDay();
+    }
+
+    public static void showAlert(String textToDisplay) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(textToDisplay);
+        alert.show();
     }
 
     @Override
@@ -87,6 +92,54 @@ public class App extends Application {
      */
     public void increaseVolume() {
         App.volume();
+    }
+
+
+    /**
+     * Switches to a new scene from an FXML file associated with input URL
+     * @param fxmlPath Path to FXML file relative to App package,
+     *            e.g. `"fxml/menu.fxml"`
+     * @throws IOException if fxmlPath is invalid
+     */
+    public void changeScene(String fxmlPath) throws IOException {
+        Parent root = FXMLLoader.load(
+            Objects.requireNonNull(getClass().getResource(fxmlPath))
+        );
+        Scene menuScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        App.stage.setScene(menuScene);
+        App.stage.show();
+    }
+
+    public void levelSelect(ActionEvent actionEvent) throws IOException {
+        // TODO: this is just here for testing without profileSelect menu
+//        GameFileHandler.loadPlayerProfile("test");
+        //
+
+        if (Game.getPlayerProfile() == null) {
+            App.showAlert("Select a profile first.");
+        } else {
+            this.changeScene(LEVEL_SELECT_FXML_PATH);
+        }
+
+
+    }
+
+    public void newGame(
+        GameParams gameParams
+    ) throws IOException, ParseBoardException, ParseTileColourException {
+        //TODO: change scene to level select, then load selected level from there
+        this.changeScene(GAME_FXML_PATH);
+        App.stage.setOnCloseRequest(t -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        App.stage.getScene().setOnKeyPressed(
+            (key) -> Game.registerNewMovementInput(key.getCode())
+        );
+
+        Thread gameThread = Game.startGame(gameParams);
+        gameThread.setPriority(6);
     }
 
     private static void volume() {
@@ -107,8 +160,8 @@ public class App extends Application {
     }
 
     private static Media loadNextTrack() {
-        Media music = new Media(new File(TRACKS[currentTrack]).toURI().toString());
-        currentTrack = currentTrack + 1 % TRACKS.length;
+        Media music = new Media(new File(TRACKS[currentAudioTrack]).toURI().toString());
+        currentAudioTrack = currentAudioTrack + 1 % TRACKS.length;
         return music;
     }
 
@@ -119,44 +172,5 @@ public class App extends Application {
             System.out.println(e.getMessage());
             App.app.messageOfTheDay.setText("There is only pain.");
         }
-    }
-
-    /**
-     * Switches to a new scene from an FXML file associated with input URL
-     * @param fxmlPath Path to FXML file relative to App package,
-     *            e.g. `"fxml/menu.fxml"`
-     * @throws IOException if fxmlPath is invalid
-     */
-    public void changeScene(String fxmlPath) throws IOException {
-        Parent root = FXMLLoader.load(
-            Objects.requireNonNull(getClass().getResource(fxmlPath))
-        );
-        Scene menuScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-        App.stage.setScene(menuScene);
-        App.stage.show();
-    }
-
-    public void newGame(
-        ActionEvent actionEvent
-    ) throws IOException, ParseBoardException, ParseTileColourException, ClassNotFoundException {
-        //TODO: change scene to level select, then load selected level from there
-        this.changeScene(GAME_FXML_PATH);
-        App.stage.setOnCloseRequest(t -> {
-            Platform.exit();
-            System.exit(0);
-        });
-
-        // TODO: this is just here for testing without level file menu
-        GameFileHandler.loadPlayerProfile("test");
-        GameParams gameParams = GameFileHandler.loadLevelFile(
-            0, Game.getPlayerProfile()
-        );
-        //
-        App.stage.getScene().setOnKeyPressed(
-            (key) -> Game.registerNewMovementInput(key.getCode())
-        );
-
-        Thread gameThread = Game.startGame(gameParams);
-        gameThread.setPriority(6);
     }
 }
--- src/Game/Game.java
+++ src/Game/Game.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.time.Instant;
-import java.util.Timer;
 
 
 public class Game {
