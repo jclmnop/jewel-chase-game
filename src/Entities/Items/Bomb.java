0e33b853bd4edd71d20a8e3c9fb5879a819486f2
Dillon Ward <dillon.ward.uk@gmail.com>
2022-12-07 18:34:29 +0000

    Implement character detection.

--- src/Entities/Items/Bomb.java
+++ src/Entities/Items/Bomb.java
@@ -1,10 +1,52 @@
 package Entities.Items;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import DataTypes.AdjacentCoords;
 import DataTypes.Coords;
+import Entities.Entity;
+import Entities.Characters.Npc.Npc;
+import Game.Tile;
 
 public class Bomb extends Item {
+
+    private int state = 4;
+    private Coords[] trigZones = Tile.getNoColourAdjacentTiles(coords).toArray();
+    private boolean triggered = false;
+
     public Bomb(Coords coords) {
         super(CollisionType.BOMB, false, coords);
+    }
+
+    /**
+     * Checks whether bomb is waiting to be triggered or is in the process of exploding.
+     */
+    public void check() {
+        if (!triggered) {
+            detect();
+        } else {
+            state--;
+            if (state == 0) {
+                explode();
+            }
+        }
+    }
+
+    /**
+     * Detects if any npcs are vertically or hozirontally next to it. If so, the bomb triggers.
+     */
+    private void detect() {
+        final int NUM_ZONES = 4;
+        int i = 0;
+        while (!triggered || i < NUM_ZONES) {
+            Coords zone = trigZones[i];
+            if (zone != null) {
+                if (!Tile.getEntitiesOfTypeByCoords(Npc.class, zone).isEmpty()) {
+                    triggered = true;
+                }
+            }
+        }
     }
 
     /**
