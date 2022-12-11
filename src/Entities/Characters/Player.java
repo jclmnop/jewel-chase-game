bbd8e104d772ca6cab28d59ef56ff93f892ab3fd
jclmnop <jcheshire@protonmail.com>
2022-12-11 11:01:05 +0000

    player javadoc

--- src/Entities/Characters/Player.java
+++ src/Entities/Characters/Player.java
@@ -6,9 +6,21 @@ import DataTypes.Direction;
 import Game.Game;
 import Game.Tile;
 
+/**
+ * Represents the Player. Game will end with a loss if there are no Player
+ * objects in the game.
+ * @author Jonny
+ * @version 1.1
+ */
 public class Player extends Character {
     private static final String IMAGE_PATH = Character.RESOURCES_PATH + "liam_stick.gif";
 
+    /**
+     * Construct a player with the given coords and ticksPerMove. Direction
+     * defaults to RIGHT.
+     * @param coords Coordinates of player.
+     * @param ticksPerMove How many ticks need to pass before each movement.
+     */
     public Player(Coords coords, int ticksPerMove) {
         super(CollisionType.PLAYER, true, coords, ticksPerMove);
         this.currentDirection = Direction.RIGHT;
@@ -16,6 +28,12 @@ public class Player extends Character {
         Game.addPlayer(this);
     }
 
+    /**
+     * Construct a player with the given coords, ticksPerMove and direction.
+     * @param coords Coordinates of player.
+     * @param ticksPerMove How many ticks need to pass before each movement.
+     * @param currentDirection Direction that the player will be facing.
+     */
     public Player(Coords coords, int ticksPerMove, Direction currentDirection) {
         super(CollisionType.PLAYER, true, coords, ticksPerMove);
         this.currentDirection = currentDirection;
@@ -23,11 +41,24 @@ public class Player extends Character {
         Game.addPlayer(this);
     }
 
+    /**
+     * Construct a player with the given coords, ticksPerMove, direction and
+     * ticksSinceLastMove. Used when deserialising from a save file. .
+     * @param coords Coordinates of player.
+     * @param ticksPerMove How many ticks need to pass before each movement.
+     * @param currentDirection Direction that the player will be facing.
+     * @param ticksSinceLastMove Number of ticks that have passed since the
+     *                           player last moved.
+     */
     public Player(Coords coords, int ticksPerMove, Direction currentDirection, int ticksSinceLastMove) {
         this(coords, ticksPerMove, currentDirection);
         this.ticksSinceLastMove = ticksSinceLastMove;
     }
 
+    /**
+     * Attempt to move in the given direction.
+     * @param direction Direction to attempt movement in.
+     */
     public void tryMove(Direction direction) {
         AdjacentCoords adjacentCoords = Tile.getMultiColourAdjacentTiles(this.coords);
         Coords to = adjacentCoords.getCoordsInDirection(direction);
@@ -35,6 +66,7 @@ public class Player extends Character {
             this.move(to);
         }
     }
+
     /**
      * Serialises the Object into a String.
      *
