5ae43d8f4b62171e52d22e687335242bca99555a
jclmnop <jcheshire@protonmail.com>
2022-12-11 11:33:36 +0000

    javadoc clock

--- src/Entities/Items/Collectable/Clock.java
+++ src/Entities/Items/Collectable/Clock.java
@@ -3,10 +3,22 @@ package Entities.Items.Collectable;
 import DataTypes.Coords;
 import Entities.Items.Item;
 
+/**
+ * Adds time to the game timer if collected by player, decreases time if collected
+ * by a thief.
+ *
+ * @author Matt
+ * @version 1.0
+ * @see Entities.Items.Collectable.Collectable
+ */
 public class Clock extends Collectable {
     public static final int SECONDS = 10;
     private static final String IMAGE_PATH = Item.RESOURCES_PATH + "clock.gif";
 
+    /**
+     * Construct a clock at the specified coordinates.
+     * @param coords
+     */
     public Clock(Coords coords) {
         super(CollisionType.CLOCK, false, coords);
         this.imagePath = IMAGE_PATH;
